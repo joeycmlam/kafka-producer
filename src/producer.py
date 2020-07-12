0af6b1413cb63f0ba5f4ea00791b68f3f2ba207f
joeylam <joey.cm.lam@gmail.com>
2020-07-12 21:41:46 +0800

    immediately flush the message to kafka server

--- src/producer.py
+++ src/producer.py
@@ -1,6 +1,19 @@
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-producer.send('sample', b'testing')
-producer.send('sample', key=b'message-two', value=b'this is first kafka python testing')
+
+if __name__ == '__main__':
+
+    print ('start')
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    print('send message')
+
+    #producer.send(topic='sample', value=b'testing-2')
+    #producer.send('sample', b'send message')
+
+    producer.send('sample', key=b'message-two', value=b'this is first kafka python testing')
+    producer.flush()
+    #myMessage = input('input message: ')
+    #producer.send('sample', value=myMessage)
+    print ('done')
 
